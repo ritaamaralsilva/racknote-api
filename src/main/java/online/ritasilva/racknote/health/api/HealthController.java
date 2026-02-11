34b49493965dd5cde21c04487f0372d1932d2cd0
ritaamaralsilva <rita.asilva93@gmail.com>
2026-02-11 03:18:20 +0000

    Expose /racknote/alive health endpoint

--- src/main/java/online/ritasilva/racknote/health/api/HealthController.java
+++ src/main/java/online/ritasilva/racknote/health/api/HealthController.java
@@ -2,12 +2,13 @@ package online.ritasilva.racknote.health.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
-    public String health() {
-        return "ok";
+    @GetMapping(value= "/racknote/alive", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String alive() {
+        return "racknote alive";
     }
 }
